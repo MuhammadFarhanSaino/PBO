604e5d08539b55e16686577108684044efedee28
MuhammadFarhanSaino <148331285+MuhammadFarhanSaino@users.noreply.github.com>
2023-10-18 16:53:57 +0700

    Add files via upload

---
+++ Menghitung Luas & Keliling Persegi Panjang.py
@@ -0,0 +1,16 @@
+print("Menghitung Luas dan Keliling Balok")
+
+# variabel
+panjang = 20
+lebar = 4
+tinggi = 10
+
+# rumus 
+luas = (2 * panjang * lebar) + (2 * panjang * tinggi) + (2 * lebar * tinggi)
+vol = (panjang * lebar * tinggi)
+
+# output 
+print("Panjang : ", panjang)
+print("Lebar : ", lebar)
+print("Luas : ", luas)
+print("Volume : ", vol)
