6631b89b8b1d0073c40c30fb2dddeadef0645bd7
MuhammadFarhanSaino <148331285+MuhammadFarhanSaino@users.noreply.github.com>
2023-10-19 14:17:26 +0700

    Add files via upload

---
+++ PBO_Pertemuan1/PERSEGI_PANJANG.py
@@ -0,0 +1,15 @@
+print("Menghitung Luas dan Keliling Persegi Panjang")
+
+# varaibel
+panjang = 20   
+lebar = 4
+
+# Rumus
+luas = panjang * lebar
+kel = (2 * panjang) + (2 * lebar)
+
+# Output
+print("Panjang : ", panjang)
+print("Lebar : ", lebar)
+print("Luas : ",luas)
+print("Keliling : ",kel) 
