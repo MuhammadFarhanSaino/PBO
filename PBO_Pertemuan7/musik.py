3220e3d594646e8cdb05af9414a5e36988c46525
MuhammadFarhanSaino <148331285+MuhammadFarhanSaino@users.noreply.github.com>
2023-12-01 11:19:30 +0700

    Add files via upload

---
+++ PBO_Pertemuan7/musik.py
@@ -0,0 +1,15 @@
+from playsound import playsound
+
+def play_audio(file_path):
+    try:
+        # Attempt to play the audio file
+        playsound(file_path)
+    except Exception as e:
+        # Handle any exceptions that occur during playback
+        print(f"Error playing audio: {e}")
+
+# Specify the file path
+audio_file_path = "women buyiyang.mp3"
+
+# Call the play_audio function with the specified file path
+play_audio(audio_file_path)
